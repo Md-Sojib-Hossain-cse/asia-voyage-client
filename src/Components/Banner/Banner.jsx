a6a19bf70108fb1de6da155cb3e32d1f956a08d2
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-04-28 13:43:17 +0600

    card animation added using AOS

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,3 +1,7 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
+
 const Banner = () => {
     return (
         <div className="-mt-20 -z-10">
@@ -5,7 +9,11 @@ const Banner = () => {
                 <div id="item1" className="carousel-item w-full">
                     <img src="https://i.ibb.co/Bwz2zsK/banner2.jpg" className="w-full object-cover" />
                     <div className="absolute h-full w-full flex justify-end items-end flex-col p-4 md:p-12">
-                        <div className=" w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
+                        <div 
+                        data-aos="fade-up"
+                        data-aos-delay="200"
+                        data-aos-easing="linear"
+                        className=" w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
                             <h4 className="text-[#FF681A] great-vibes text-2xl font-medium">Tropical Tranquility</h4>
                             <p className="text-white font-normal text-base">Escape to the serene beauty of palm-fringed riverbanks, where the gentle flow of water whispers tales of relaxation and rejuvenation.</p>
                             <ul className="text-white text-sm font-medium list-disc list-inside">
@@ -19,7 +27,11 @@ const Banner = () => {
                 <div id="item2" className="carousel-item w-full">
                     <img src="https://i.ibb.co/ZhhXDcT/banner3.jpg" className="w-full object-cover" />
                     <div className="absolute h-full w-full flex justify-end items-end flex-col p-4 md:p-12">
-                        <div className=" w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
+                        <div 
+                        data-aos="fade-up"
+                        data-aos-delay="200"
+                        data-aos-easing="linear"
+                        className="w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
                             <h4 className="text-[#FF681A] great-vibes text-2xl font-medium">Arctic Wonderland</h4>
                             <p className="text-white font-normal text-base">Set off on an adventure to an enchanting Arctic wonderland, where icy peaks cradle a pristine lake, creating a scene reminiscent of a winter fairy tale.</p>
                             <ul className="text-white text-sm font-medium list-disc list-inside">
@@ -33,7 +45,11 @@ const Banner = () => {
                 <div id="item3" className="carousel-item w-full">
                     <img src="https://i.ibb.co/gjFkW6H/banner1.png" className="w-full object-cover" />
                     <div className="absolute h-full w-full  flex justify-end items-end flex-col p-4 md:p-12">
-                        <div className=" w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
+                        <div 
+                        data-aos="fade-up"
+                        data-aos-delay="200"
+                        data-aos-easing="linear"
+                        className=" w-full md:w-2/3 lg:w-1/2 bg-[#13131344] p-4 md:p-8 rounded-lg drop-shadow-lg shadow-lg backdrop-blur-sm">
                             <h4 className="text-[#FF681A] great-vibes text-2xl font-medium">Serenity in Natures Embrace</h4>
                             <p className="text-white font-normal text-base">Immerse yourself in the tranquility of a secluded lake nestled amidst rolling hills and verdant forests, offering a sanctuary of peace and natural beauty.</p>
                             <ul className="text-white text-sm font-medium list-disc list-inside">
