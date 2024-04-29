f5ce590744f6a6f1182b398314096881b0b1fe31
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-04-29 22:32:18 +0600

    website theme toggle added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const Navbar = () => {
     const { logoutUser, user, setUser, loading } = useContext(AuthContext);
+    const [ theme , setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+
 
     //handle logout user 
     const handleLogOut = () => {
@@ -13,6 +16,29 @@ const Navbar = () => {
                 setUser(null)
             )
     }
+
+    const handleTheme = (e) => {
+        if(e.target.checked){
+            setTheme("sunset")
+        }
+        else{
+            setTheme("light")
+        }
+    }
+
+    // theme controller
+    useEffect(() => {
+        localStorage.setItem("theme" , theme);
+        const localTheme = localStorage.getItem("theme")
+        document.querySelector("html").setAttribute("data-theme" , localTheme)
+    } ,[theme])
+
+    const themeController = <>
+        <label className="cursor-pointer grid place-items-center">
+            <input onChange={handleTheme} id="themeController" type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+            <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+            <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+        </label></>
 
 
     const navLinks = <>
@@ -65,7 +91,8 @@ const Navbar = () => {
                     loading ?
                         <span className="loading loading-spinner loading-md"></span> :
                         user ?
-                            <div className="navbar-end">
+                            <div className="navbar-end space-x-3">
+                                {themeController}
                                 <div className="dropdown dropdown-hover">
                                     <img src={user?.photoURL || "https://i.ibb.co/sbkyhCp/blank-profile.png"} alt="" className="h-8 w-8 rounded-full border border-[#131313]" />
                                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 relative right-0">
@@ -74,9 +101,9 @@ const Navbar = () => {
                                     </ul>
                                 </div>
 
-
                             </div> :
-                            <div className="navbar-end gap-3">
+                            <div className="navbar-end gap-3 space-x-3">
+                                {themeController}
                                 <Link to="/register"><button className="btn rounded-lg bg-[#FF681A] shadow-lg drop-shadow-lg text-white font-medium border-none">Register</button></Link>
                                 <Link to="/login"><button className="btn rounded-lg bg-[#FF681A] shadow-lg drop-shadow-lg text-white font-medium border-none">Login</button></Link>
                             </div>
